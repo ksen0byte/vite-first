fcee7f45ba415a0af2c4672ed4d251c224aa2f7f
Pavlo Phanel <devpavlophanel@gmail.com>
2025-01-06 17:08:51 +0100

    only letters are allowed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import {setupSettingsScreen} from "./settings-screen";
 import {setupLanguageToggle, updateLanguageUI} from "./ui";
 
+const inputChecks = document.querySelectorAll(".inputCheck") as NodeListOf<HTMLInputElement>;
+
+inputChecks.forEach(inputCheck => {
+    inputCheck.addEventListener("input", checking);
+});
+
+function checking(this: HTMLInputElement) {
+    this.value = this.value.replace(/[^А-Яа-яЇїЄєІіҐґa-zA-Z]/g, '');
+}
+
 // Initialize the UI
 updateLanguageUI();
 setupLanguageToggle("language-toggle");
