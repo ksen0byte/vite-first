71dce8282a2637a21538630fb1139e12c984380b
ksen0byte <t989898@gmail.com>
2025-01-07 03:00:58 +0200

    refactor ts

---
+++ src/config/settings-screen-config.ts
@@ -0,0 +1,124 @@
+import {Options} from "nouislider";
+
+const intFormatter = {
+  to: (value: number): string => Math.round(value).toString(),
+  from: (value: string): number => parseInt(value, 10),
+};
+
+export interface SliderConfig {
+  id: string,
+  options: Options,
+  label: {
+    id: string,
+    localizationKey: string,
+    unit: string,
+  }
+}
+
+export const subsectionsConfig = {
+  shape: {
+    sizeSlider: {
+      id: "shape-size-slider",
+      options: {
+        start: 50,
+        step: 10,
+        connect: "lower",
+        range: {min: 20, max: 70},
+        // pips: { mode: PipsMode.Values, values: [20, 30, 40, 50, 60, 70], density: 10 },
+        format: intFormatter,
+      } as Options,
+      label: {
+        id: "shape-size-slider-label",
+        localizationKey: "shapeSizeSliderLabel",
+        unit: "mm",
+      }
+    } as SliderConfig,
+  },
+  word: {
+    sizeSlider: {
+      id: "word-size-slider",
+      options: {
+        start: 20,
+        step: 1,
+        connect: "lower",
+        range: {min: 15, max: 30},
+        // pips: { mode: PipsMode.Values, values: [20, 30, 40, 50, 60, 70], density: 10 },
+        format: intFormatter,
+      } as Options,
+      label: {
+        id: "word-size-slider-label",
+        localizationKey: "wordSizeSliderLabel",
+        unit: "mm",
+      }
+    }
+  },
+  syllable: {
+    sizeSlider: {
+      id: "syllable-size-slider",
+      options: {
+        start: 20,
+        step: 1,
+        connect: "lower",
+        range: {min: 15, max: 30},
+        // pips: { mode: PipsMode.Values, values: [20, 30, 40, 50, 60, 70], density: 10 },
+        format: intFormatter,
+      } as Options,
+      label : {
+        id: "syllable-size-slider-label",
+        localizationKey: "syllableSizeSliderLabel",
+        unit: "mm",
+      }
+    }
+  },
+  general: {
+    exposureTimeSlider: {
+      id: "exposure-time-slider",
+      label: {
+        id: "exposure-time-label",
+        localizationKey: "exposureTimeLabel",
+        unit: "ms",
+      },
+      options: {
+        start: 700,
+        step: 50,
+        connect: "lower",
+        range: {min: 500, max: 1000},
+        // pips: { mode: PipsMode.Positions, values: [0, 25, 50, 75, 100], density: 5 },
+        format: intFormatter,
+      },
+    } as SliderConfig,
+    exposureDelaySlider: {
+      id: "exposure-delay-slider",
+      label: {
+        id: "exposure-delay-label",
+        localizationKey: "exposureDelayLabel",
+        unit: "ms",
+      },
+      options: {
+        start: [750, 1250],
+        step: 50,
+        connect: true,
+        range: {min: 500, max: 1500},
+        // pips: { mode: PipsMode.Positions, values: [0, 25, 50, 75, 100], density: 5 },
+        format: intFormatter,
+      },
+    } as SliderConfig,
+    stimulusCountSlider: {
+      id: "stimulus-count-slider",
+      label: {
+        id: "stimulus-count-label",
+        localizationKey: "exposureDelayLabel",
+        unit: "ms",
+      },
+      options: {
+        start: 50,
+        step: 2,
+        connect: "lower",
+        range: {min: 30, max: 100},
+        // pips: { mode: PipsMode.Positions, values: [0, 25, 50, 75, 100], density: 5 },
+        format: intFormatter,
+      },
+    } as SliderConfig
+  }
+
+}
--- src/settings-screen.ts
+++ src/settings-screen.ts
@@ -1,61 +1,62 @@
 import {localize} from "./ui.ts";
-import noUiSlider, {Options, target} from 'nouislider';
+import noUiSlider, {target} from 'nouislider';
 import 'nouislider/dist/nouislider.css';
+import {SliderConfig, subsectionsConfig} from "./config/settings-screen-config.ts";
 
 export function setupSettingsScreen(containerId: string): void {
-  const container = document.getElementById(containerId)!;
+  const container: HTMLElement = document.getElementById(containerId)!;
 
-  // Subsection toggle
-  const shapesSubsection = container.querySelector("#shapes-subsection") as HTMLElement;
-  const wordsSubsection = container.querySelector("#words-subsection") as HTMLElement;
-  const syllablesSubsection = container.querySelector("#syllables-subsection") as HTMLElement;
+  setupSubsectionToggles(
+    [
+      container.querySelector("#shapes-subsection")!,
+      container.querySelector("#words-subsection")!,
+      container.querySelector("#syllables-subsection")!
+    ]
+  );
 
-  [shapesSubsection, wordsSubsection, syllablesSubsection].forEach(subSection => {
+  setupGeometricShapeSection();
+  setupWordsSection();
+  setupSyllablesSection();
+}
+
+
+const setupSubsectionToggles = (subsections: HTMLElement[]) => {
+  subsections.forEach(subSection => {
     const header = subSection.querySelector(".subsection-header") as HTMLElement;
     header.addEventListener("click", () => {
       // Collapse all
-      [shapesSubsection, wordsSubsection, syllablesSubsection].forEach(s => s.classList.remove("expanded"));
-
+      subsections.forEach(s => s.classList.remove("expanded"));
       // Mark chosen and expand
       subSection.classList.add("expanded");
     });
   });
+}
 
-  /********************************************/
-  /* 0. SLIDERS                               */
-  /********************************************/
-  const intFormatter = {
-    to: (value: number): string => Math.round(value).toString(),
-    from: (value: string): number => parseInt(value, 10),
-  };
-  const exposureTimeSliderOptions: Options = {
-    start: 700,
-    step: 50,
-    connect: "lower",
-    range: {min: 500, max: 1000},
-    // pips: { mode: PipsMode.Positions, values: [0, 25, 50, 75, 100], density: 5 },
-    format: intFormatter,
-  };
-  const exposureDelaySliderOptions: Options = {
-    start: [750, 1250],
-    step: 50,
-    connect: true,
-    range: {min: 500, max: 1500},
-    // pips: { mode: PipsMode.Positions, values: [0, 25, 50, 75, 100], density: 5 },
-    format: intFormatter,
-  };
-  const stimulusCountSliderOptions : Options= {
-    start: 50,
-    step: 2,
-    connect: "lower",
-    range: {min: 30, max: 100},
-    // pips: { mode: PipsMode.Positions, values: [0, 25, 50, 75, 100], density: 5 },
-    format: intFormatter,
-  };
+const setupSlider = (
+  sliderConfig: SliderConfig,
+  idPrefix: string = "",
+  onUpdate: (size: number, secondValue?: number) => void = () => {
+  }
+) => {
+  const slider = document.getElementById(idPrefix + sliderConfig.id)! as target;
+  const label = document.getElementById(idPrefix + sliderConfig.label.id)! as target;
 
-  /********************************************/
-  /* 1. SHAPES                                */
-  /********************************************/
+  noUiSlider.create(slider, sliderConfig.options);
+  slider.noUiSlider!.on("update", (values, _) => {
+    const firstValue = Number(values[0]);
+    const secondValue = values[1] !== undefined ? Number(values[1]) : undefined;
+    if (secondValue !== undefined) {
+      label.textContent = `${localize(sliderConfig.label.localizationKey)}: ${firstValue}-${secondValue} ${sliderConfig.label.unit}`;
+      onUpdate(firstValue);
+    } else {
+      label.textContent = `${localize(sliderConfig.label.localizationKey)}: ${firstValue} ${sliderConfig.label.unit}`;
+      onUpdate(firstValue, secondValue);
+    }
+  });
+
+}
+
+const setupGeometricShapeSection = () => {
   const redCircleShape = {
     name: "Red Circle",
     svg: `<svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet"><circle cx="50" cy="50" r="40" fill="red"/></svg>`,
@@ -72,132 +73,29 @@ export function setupSettingsScreen(containerId: string): void {
     svgElement.setAttribute("height", (size / 0.8).toString() + "mm");
   }
 
-  const shapeSizeSlider = document.getElementById("shape-size-slider")! as target;
-  const shapesExposureTimeSlider = document.getElementById("shapes-exposure-time-slider")! as target;
-  const shapesExposureDelaySlider = document.getElementById("shapes-exposure-delay-slider")! as target;
-  const shapesStimulusCountSlider = document.getElementById("shapes-stimulus-count-slider")! as target;
+  const sectionPrefix = "shapes-";
+  setupSlider(subsectionsConfig.shape.sizeSlider, "", (size) => showRedCircle(size));
+  setupSlider(subsectionsConfig.general.exposureTimeSlider, sectionPrefix);
+  setupSlider(subsectionsConfig.general.exposureDelaySlider, sectionPrefix);
+  setupSlider(subsectionsConfig.general.stimulusCountSlider, sectionPrefix);
+}
 
-  noUiSlider.create(shapeSizeSlider, {
-    start: 50,
-    step: 10,
-    connect: "lower",
-    range: {min: 20, max: 70},
-    // pips: { mode: PipsMode.Values, values: [20, 30, 40, 50, 60, 70], density: 10 },
-    format: intFormatter,
-  });
+const setupWordsSection = () => {
+  const setWordSize = (size: number) => document.getElementById("word-preview-word")!.style.fontSize = size + "mm"
 
-  noUiSlider.create(shapesExposureTimeSlider, exposureTimeSliderOptions);
-  noUiSlider.create(shapesExposureDelaySlider, exposureDelaySliderOptions);
-  noUiSlider.create(shapesStimulusCountSlider, stimulusCountSliderOptions);
+  const sectionPrefix = "words-";
+  setupSlider(subsectionsConfig.word.sizeSlider, "", (size) => setWordSize(size));
+  setupSlider(subsectionsConfig.general.exposureTimeSlider, sectionPrefix);
+  setupSlider(subsectionsConfig.general.exposureDelaySlider, sectionPrefix);
+  setupSlider(subsectionsConfig.general.stimulusCountSlider, sectionPrefix);
+}
 
-  shapeSizeSlider.noUiSlider!.on("update", (values, _) => {
-    const sizeLabel = document.getElementById("shapes-size-slider-label")!;
-    let size = Number(values[0]);
-    sizeLabel.textContent = `${localize("shapeSizeSliderLabel")}: ${size} mm`;
-    showRedCircle(size);
-  });
+const setupSyllablesSection = () => {
+  const setSyllableSize = (size: number) => document.getElementById("syllable-preview-syllable")!.style.fontSize = size + "mm"
 
-  shapesExposureTimeSlider.noUiSlider!.on("update", (values, _) => {
-    const timeLabel = document.getElementById("shapes-exposure-time-label")!;
-    timeLabel.textContent = `${localize("exposureTimeLabel")}: ${values[0]} ms`;
-  });
-
-  shapesExposureDelaySlider.noUiSlider!.on("update", (values, _) => {
-    const delayLabel = document.getElementById("shapes-exposure-delay-label")!;
-    delayLabel.textContent = `${localize("exposureDelayLabel")}: ${values[0]} - ${values[1]} ms`;
-  });
-
-  shapesStimulusCountSlider.noUiSlider!.on("update", (values, _) => {
-    const delayLabel = document.getElementById("shapes-stimulus-count-label")!;
-    delayLabel.textContent = `${localize("stimulusCountLabel")}: ${values[0]} shapes`;
-  });
-
-  /********************************************/
-  /* 2. WORDS                                 */
-  /********************************************/
-  const wordSizeSlider = document.getElementById("word-size-slider")! as target;
-  const wordsExposureTimeSlider = document.getElementById("words-exposure-time-slider")! as target;
-  const wordsExposureDelaySlider = document.getElementById("words-exposure-delay-slider")! as target;
-  const wordsStimulusCountSlider = document.getElementById("words-stimulus-count-slider")! as target;
-  const wordPreviewWord = document.getElementById("word-preview-word") as HTMLElement;
-
-  noUiSlider.create(wordSizeSlider, {
-    start: 20,
-    step: 1,
-    connect: "lower",
-    range: {min: 15, max: 30},
-    // pips: { mode: PipsMode.Values, values: [20, 30, 40, 50, 60, 70], density: 10 },
-    format: intFormatter,
-  });
-
-  noUiSlider.create(wordsExposureTimeSlider, exposureTimeSliderOptions);
-  noUiSlider.create(wordsExposureDelaySlider, exposureDelaySliderOptions);
-  noUiSlider.create(wordsStimulusCountSlider, stimulusCountSliderOptions);
-
-  wordSizeSlider.noUiSlider!.on("update", (values, _) => {
-    const sizeLabel = document.getElementById("words-size-slider-label")!;
-    let size = Number(values[0]);
-    sizeLabel.textContent = `${localize("wordSizeSliderLabel")}: ${size} mm`;
-    wordPreviewWord.style.fontSize = size + "mm";
-  });
-
-  wordsExposureTimeSlider.noUiSlider!.on("update", (values, _) => {
-    const timeLabel = document.getElementById("words-exposure-time-label")!;
-    timeLabel.textContent = `${localize("exposureTimeLabel")}: ${values[0]} ms`;
-  });
-
-  wordsExposureDelaySlider.noUiSlider!.on("update", (values, _) => {
-    const delayLabel = document.getElementById("words-exposure-delay-label")!;
-    delayLabel.textContent = `${localize("exposureDelayLabel")}: ${values[0]} - ${values[1]} ms`;
-  });
-
-  wordsStimulusCountSlider.noUiSlider!.on("update", (values, _) => {
-    const delayLabel = document.getElementById("words-stimulus-count-label")!;
-    delayLabel.textContent = `${localize("stimulusCountLabel")}: ${values[0]} words`;
-  });
-
-
-  /********************************************/
-  /* 3. Syllables                             */
-  /********************************************/
-  const syllableSizeSlider = document.getElementById("syllable-size-slider")! as target;
-  const syllablesExposureTimeSlider = document.getElementById("syllables-exposure-time-slider")! as target;
-  const syllablesExposureDelaySlider = document.getElementById("syllables-exposure-delay-slider")! as target;
-  const syllablesStimulusCountSlider = document.getElementById("syllables-stimulus-count-slider")! as target;
-  const syllablePreviewSyllable = document.getElementById("syllable-preview-syllable") as HTMLElement;
-
-  noUiSlider.create(syllableSizeSlider, {
-    start: 20,
-    step: 1,
-    connect: "lower",
-    range: {min: 15, max: 30},
-    // pips: { mode: PipsMode.Values, values: [20, 30, 40, 50, 60, 70], density: 10 },
-    format: intFormatter,
-  });
-
-  noUiSlider.create(syllablesExposureTimeSlider, exposureTimeSliderOptions);
-  noUiSlider.create(syllablesExposureDelaySlider, exposureDelaySliderOptions);
-  noUiSlider.create(syllablesStimulusCountSlider, stimulusCountSliderOptions);
-
-  syllableSizeSlider.noUiSlider!.on("update", (values, _) => {
-    const sizeLabel = document.getElementById("syllables-size-slider-label")!;
-    let size = Number(values[0]);
-    sizeLabel.textContent = `${localize("syllableSizeSliderLabel")}: ${size} mm`;
-    syllablePreviewSyllable.style.fontSize = size + "mm";
-  });
-
-  syllablesExposureTimeSlider.noUiSlider!.on("update", (values, _) => {
-    const timeLabel = document.getElementById("syllables-exposure-time-label")!;
-    timeLabel.textContent = `${localize("exposureTimeLabel")}: ${values[0]} ms`;
-  });
-
-  syllablesExposureDelaySlider.noUiSlider!.on("update", (values, _) => {
-    const delayLabel = document.getElementById("syllables-exposure-delay-label")!;
-    delayLabel.textContent = `${localize("exposureDelayLabel")}: ${values[0]} - ${values[1]} ms`;
-  });
-
-  syllablesStimulusCountSlider.noUiSlider!.on("update", (values, _) => {
-    const delayLabel = document.getElementById("syllables-stimulus-count-label")!;
-    delayLabel.textContent = `${localize("stimulusCountLabel")}: ${values[0]} syllables`;
-  });
+  const sectionPrefix = "syllables-";
+  setupSlider(subsectionsConfig.syllable.sizeSlider, "", (size) => setSyllableSize(size));
+  setupSlider(subsectionsConfig.general.exposureTimeSlider, sectionPrefix);
+  setupSlider(subsectionsConfig.general.exposureDelaySlider, sectionPrefix);
+  setupSlider(subsectionsConfig.general.stimulusCountSlider, sectionPrefix);
 }
