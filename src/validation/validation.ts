1e5c8796656aacf6499acc079b83719bbc86fff5
ksen0byte <t989898@gmail.com>
2025-01-07 03:01:24 +0200

    prep validation.ts

---
+++ src/validation/validation.ts
@@ -0,0 +1,22 @@
+const validateFirstNameInput = () => {
+  // do the stuff
+};
+
+const validateLastNameInput = () => {
+  // do the stuff
+};
+
+const validateAgeInput = () => {
+  // do the stuff
+};
+
+const validateGenderInput = () => {
+  // do the stuff
+};
+
+export const validateSettingScreenInputs = () => {
+  validateFirstNameInput();
+  validateLastNameInput();
+  validateAgeInput();
+  validateGenderInput();
+}
